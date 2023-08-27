12abe7d092e856bbca3426d1f35b8f1421a705ea
avegamers <jonio13lp@gmail.com>
2023-08-27 20:37:18 +0200

    added ping command and command builder

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const botCreator = "AveGamers"; //The Name of the Bot Creator. Its just a way to use it in the code.
 
 const fs = require('fs'); //File System   //This is a way to use the File System in Node.js
-const Discord = require('discord.js'); //Discord.js   //This is a way to use the Discord.js in Node.js
+const path = require('node:path'); //Node Path   //This is a way to use the Node Path in Node.js
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js'); //Discord.js   //This is a way to use the Discord.js in Node.js
 const config = require('./config.json'); //Config.json   //This is a way to use the Config.json in Node.js
 
-import { Client, GatewayIntentBits } from 'discord.js';
 const client = new Client({
 
     fetchAllMembers: false,
@@ -14,14 +14,12 @@ const client = new Client({
     parse: ["roles", "users"],
     repliedUser: false,
   },
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
-  intents: [ 
-    Discord.Intents.FLAGS.GUILDS,
-     Discord.Intents.FLAGS.GUILD_MEMBERS,
-    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-    Discord.Intents.FLAGS.GUILD_MESSAGES,
-  ],
+  intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
   presence: {
     activities: [{
       name: `${config.status.text}`.replace("{prefix}", config.prefix), 
@@ -29,13 +27,57 @@ const client = new Client({
     }],
     status: "online"
   }
-
-
 }); //Discord Client   //This is a way to use the Discord Client in Node.js
 
 
 
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
+
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 
 client.on('ready', () => { //Discord Client Ready Event
